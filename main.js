3d1762a01ad91c63fea7bb2d538883056a816f03
Larissa <lari.nih@hotmail.com>
2022-03-08 21:59:09 -0400

    estilizacao quase finalizada

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-import './style.css'
+//import './style.css'
 
+const $ = document.querySelector.bind(document)
+
+const addTask = (event) => {
+
+  event.preventDefault()
+  const taskDescription = $('#task-description').value 
+
+  const newTaskItem = `
+    <li>${taskDescription}</li>
+  `
+
+  $('#task-list').insertAdjacentHTML('beforeend', newTaskItem)
+  $('#task-form').reset()
+}
